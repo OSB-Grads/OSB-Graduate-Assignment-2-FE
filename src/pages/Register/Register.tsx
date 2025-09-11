24ab80a0710d6931be5a4623570ca319151a622c
AnushaD08 <122457845+AnushaD08@users.noreply.github.com>
2025-09-11 16:47:31 +0530

    userStore : Added the userstore (#13)
    * Added the userstore
    
    * Resolved the comments

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -4,6 +4,8 @@ import ButtonComponent from "../../components/Button/ButtonComponent.tsx";
 import InputField from "../../components/inputField/inputField.tsx";
 import { Link, useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/httpClientUtil.ts";
+
+import { getUserStore } from "../../store/userstore/userstoreGetters.ts";
 import useAuthStore from "../../store/AuthStore/authStore.ts";
 
 
@@ -19,6 +21,7 @@ function Register() {
   const navigate = useNavigate();
 
   const { isAuthenticated, signup } = useAuthStore();
+  const{createUser}=getUserStore();
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,13 +33,9 @@ function Register() {
 
     try {
       await signup(username, password);
-      const userDetails = await axiosInstance.put("/api/v1/users/me", {
-        name,
-        email,
-        phone,
-      });
+      const status = await createUser(name,email,phone);
 
-      if (userDetails.status === 200 || userDetails.status === 201) {
+      if (status === 200 || status === 201) {
         console.log("Successful"); // will be updated with toast
       }
 
---
+++ src/store/userstore/userstore.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+import { createUser, getUser } from "./userstore.logic";
+import type { UserState } from "./userstore.interface";
+
+const UserStore=create<UserState>()(
+    (set)=>({
+        user:undefined,
+        createUser:(name,email,phone)=>createUser(set,name,email,phone),
+        getUser:()=>getUser(set),
+
+    })
+)
+export default UserStore;
