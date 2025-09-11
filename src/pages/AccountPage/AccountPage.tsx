4a07492d64f56a60dfe95a4bc0da8a2df5a10559
AJadiyappa <desaianushadesai@gmail.com>
2025-09-11 16:59:18 +0530

    resolved the commits

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -4,6 +4,25 @@ import ButtonComponent from "../../components/Button/ButtonComponent";
 
 import './AccountPage.css'
 import TableComponent from "../../components/TableComponent/TableComponent";
+
+
+interface AccountData {
+  accountNumber: string;
+  accountType: string;
+  balance: number;
+  accountCreated: string;
+  accountUpdated: string;
+}
+
+const defaultAccounts: AccountData[] = [
+  {
+    accountNumber: "1234567890",
+    accountType: "Savings",
+    balance: 2500.75,
+    accountCreated: "2022-01-15T10:00:00",
+    accountUpdated: "2025-09-09T12:30:00"
+  }]
+
 const AccountPage = () => {
 
   const tableheader = ["Name", "Email", "Action"];
