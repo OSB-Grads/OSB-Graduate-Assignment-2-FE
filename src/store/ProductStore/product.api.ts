2348df14091862469e30c6faf6f6d7b58c73b650
SaiGanesh-Gattu <saiganeshgattu123@gmail.com>
2025-10-09 11:56:30 +0530

    GEN-165: Creation Of Docker File

--- src/store/ProductStore/product.api.ts
+++ src/store/ProductStore/product.api.ts
@@ -2,5 +2,5 @@ import axiosInstance from "../../utils/httpClientUtil";
 
 
 export const apiCallToTheProductsEndpoint=async()=>{
-    return await axiosInstance.get(import.meta.env.VITE_API_BASE_URL + '/api/v1/product/fetch');
+    return await axiosInstance.get('/api/v1/product/fetch');
 }
