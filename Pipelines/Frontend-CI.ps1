d90ca144578d5fc4830998ca951ec872db9eed6e
MChinnappa <monicaglory05@gmail.com>
2025-11-05 16:00:13 +0530

    Resolved 05

--- Pipelines/Frontend-CI.ps1
+++ Pipelines/Frontend-CI.ps1
@@ -5,24 +5,17 @@ function Log($message) {
     Start-Sleep -Milliseconds 100
 }
 
-# Args
+# Arguments
 $ACR_NAME = $args[0]
 $IMAGE_NAME = $args[1]
 $DOCKERFILE_PATH = $args[2]
 $IMAGE_TAG = $args[3]
-
-# Read backend URL from .env file
-# $envFilePath = "$(Build.SourcesDirectory)\.env"
-# $backendUrl = (Get-Content $envFilePath | Select-String "VITE_API_BASE_URL").ToString().Split("=")[1].Trim()
-
-Log "Backend URL detected: $backendUrl"
 
 Log "Logging into ACR..."
 az acr login --name $ACR_NAME
 
 Log "Building Docker image..."
 docker build `
-  --build-arg VITE_API_BASE_URL=$backendUrl `
   -t "${ACR_NAME}.azurecr.io/${IMAGE_NAME}:${IMAGE_TAG}" `
   -f $DOCKERFILE_PATH .
 
